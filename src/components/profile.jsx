ce7aba02c4076d7e001b714384ec17602625440c
Lee Inji <lij8016@gmail.com>
2022-09-01 23:39:29 +0900

    Toggle switch 기능 추가

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 const Profile = (props) => {
   return (
     <>
-      <i className="fa-solid fa-bell"></i>
-      <button>이</button>
+      <div className="profile-container">
+        <div className="toggle-theme-wrapper">
+          <input type="checkbox" id="checkbox" />
+          <label className="switch" htmlFor="checkbox">
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <button className="alram">
+          <i className="fa-regular fa-bell"></i>
+        </button>
+        <button className="profile-name">이</button>
+      </div>
     </>
   );
 };
