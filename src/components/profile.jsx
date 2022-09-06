95075c08a208d88b2675b673813c991496c128e4
Lee Inji <lij8016@gmail.com>
2022-09-06 16:59:47 +0900

    🥅 DarkMode 정상 구현

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 
 const Profile = () => {
+  const setDark = () => {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+  };
+
+  const setLight = () => {
+    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
+  };
+
+  const theme = localStorage.getItem('theme');
+  const preferDark = window.matchMedia('(prefers-color-scheme:dark)').matches
+    ? 'dark'
+    : 'light';
+
+  const getUserTheme = () => (theme ? theme : preferDark);
+
+  window.onload = function () {
+    console.log('loaded');
+    if (getUserTheme === 'dark') {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  };
+
+  function toggleTheme(e) {
+    const className = e.target.checked;
+    if (className) {
+      setDark();
+    } else {
+      setLight();
+    }
+  }
   return (
     <>
       <div className="profile-container">
         <div className="toggle-theme-wrapper">
-          <input type="checkbox" id="checkbox" className="checkbox" />
+          <input
+            type="checkbox"
+            id="checkbox"
+            className="checkbox"
+            onChange={toggleTheme}
+          />
           <label className="switch" htmlFor="checkbox">
             <span className="slider round"></span>
           </label>
