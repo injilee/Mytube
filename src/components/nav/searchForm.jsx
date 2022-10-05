20ba49a58c3e94f859d36e4089caeb0cbc1f5d48
Lee Inji <lij8016@gmail.com>
2022-10-05 23:40:04 +0900

    search 기능 구현

--- src/components/nav/searchForm.jsx
+++ src/components/nav/searchForm.jsx
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import styles from './searchFrom.module.css';
 
-export default class SearchForm extends Component {
-  render() {
-    return (
-      <form action="" className={styles.searchForm}>
-        <div className={styles.searchContainer}>
-          <input
-            type="text"
-            placeholder="Search"
-            className={styles.searchInput}
-          />
-          <button className={styles.searchBtn}>
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </button>
-        </div>
-      </form>
-    );
-  }
-}
+const SearchForm = ({ onSearch }) => {
+  const textInput = useRef();
+
+  const handleSearch = () => {
+    const value = textInput.current.value;
+    onSearch(value);
+    textInput.current.value = '';
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        type="text"
+        placeholder="Search"
+        className={styles.searchInput}
+        onKeyPress={onKeyPress}
+        ref={textInput}
+      />
+      <button type="submit" className={styles.searchBtn} onClick={onClick}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
+  );
+};
+
+export default SearchForm;
