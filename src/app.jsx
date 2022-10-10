96fca9b1daca8a650c695c8402d0b4098c24e28f
Lee Inji <lij8016@gmail.com>
2022-10-10 23:52:19 +0900

    nav component 스타일 수정

--- src/app.jsx
+++ src/app.jsx
@@ -2,29 +2,26 @@ import React, { useEffect, useState } from 'react';
 import './app.css';
 import NavBar from './components/nav/navBar';
 import VideoList from './components/video_list/videoList';
+import ViewDetail from './components/view/viewDetail';
 
 const App = ({ youtube }) => {
   const [videos, getVideos] = useState([]);
+  // const [view, getViews] = useState([]);
 
   const search = (query) => {
-    youtube
-      .search(query)
-      .then((items) => getVideos(items))
-      .catch((error) => console.log('error', error));
+    youtube.search(query).then((items) => getVideos(items));
   };
 
   // 첫 화면에 보여지는 동영상 목록이기 때문에 mount 되자마자 받아옴.
   useEffect(() => {
-    youtube
-      .mostPopular()
-      .then((items) => getVideos(items))
-      .catch((error) => console.log('error', error));
+    youtube.mostPopular().then((items) => getVideos(items));
   }, [youtube]);
 
   return (
     <>
       <NavBar onSearch={search}></NavBar>
-      <VideoList videoItem={videos}></VideoList>
+      {/* <VideoList videoItem={videos}></VideoList> */}
+      <ViewDetail onView={youtube}></ViewDetail>
     </>
   );
 };
